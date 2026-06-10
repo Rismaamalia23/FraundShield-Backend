cc345c4625c49329c765de96da7b4b69c505fa3e
Rismaamalia23 <2311103031@ittelkom-pwt.ac.id>
2026-06-11 00:37:32 +0700

    feat: make fraud score evaluation dynamic using keywords from database rules

--- src/services/fraudDetectionService.js
+++ src/services/fraudDetectionService.js
@@ -1,35 +1,48 @@
 const transactionModel = require('../models/transactionModel');
+const fraudRuleModel = require('../models/fraudRuleModel');
 
 const calculateFraudScore = async (transactionData) => {
   let score = 0;
   const { user_id, amount, location, transaction_time } = transactionData;
   const time = new Date(transaction_time);
-
-  // 1. Jika nominal transaksi > 10.000.000 -> score +40
-  if (parseFloat(amount) > 10000000) {
-    score += 40;
-  }
-
-  // 2. Jika frekuensi transaksi > 5 kali -> score +30
-  // Note: we'll check total transactions by user or within a timeframe.
-  // We'll just check total transactions as a simple condition for now, since no timeframe was specified.
-  const txCount = await transactionModel.getTransactionCountByUser(user_id);
-  if (txCount > 5) {
-    score += 30;
-  }
-
-  // 3. Jika lokasi berbeda -> score +20
-  const lastLocationInfo = await transactionModel.getLastTransactionLocation(user_id);
-  if (lastLocationInfo && lastLocationInfo.location) {
-    if (lastLocationInfo.location.toLowerCase() !== location.toLowerCase()) {
-      score += 20;
-    }
-  }
-
-  // 4. Jika transaksi jam 00.00-04.00 -> score +10
   const hour = time.getHours();
-  if (hour >= 0 && hour <= 4) {
-    score += 10;
+
+  // 1. Ambil semua rules dari database
+  const rules = await fraudRuleModel.getFraudRules();
+
+  // 2. Siapkan data tambahan (query di luar loop agar efisien)
+  const txCount = await transactionModel.getTransactionCountByUser(user_id);
+  const lastLocationInfo = await transactionModel.getLastTransactionLocation(user_id);
+
+  // 3. Evaluasi setiap rule berdasarkan condition_type (keyword)
+  for (const rule of rules) {
+    const type = rule.condition_type.toLowerCase();
+
+    switch (type) {
+      case 'amount_high':
+        if (parseFloat(amount) > 10000000) score += rule.risk_point;
+        break;
+
+      case 'high_frequency':
+        if (txCount > 5) score += rule.risk_point;
+        break;
+
+      case 'location':
+        if (lastLocationInfo && lastLocationInfo.location) {
+          if (lastLocationInfo.location.toLowerCase() !== location.toLowerCase()) {
+            score += rule.risk_point;
+          }
+        }
+        break;
+
+      case 'night_time':
+        if (hour >= 0 && hour <= 4) score += rule.risk_point;
+        break;
+        
+      default:
+        // Jika condition_type tidak dikenali sistem, abaikan.
+        break;
+    }
   }
 
   return score;
