8543452b50e46cbec9b4268fddd29bc4091fba66
Rismaamalia23 <2311103031@ittelkom-pwt.ac.id>
2026-06-11 00:19:30 +0700

    fix: change get users endpoint to root and allow analis access

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,16 +4,8 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const checkRole = require('../middlewares/roleMiddleware');
 const userController = require('../controllers/userController');
 
-// a) Endpoint hanya untuk ADMIN
-router.get('/admin', authMiddleware, checkRole(['admin']), userController.getAllUsers);
-
-// b) Endpoint untuk ADMIN dan ANALIS
-router.get('/analis', authMiddleware, checkRole(['admin', 'analis']), (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Akses untuk admin dan analis'
-  });
-});
+// a) Endpoint untuk melihat semua user (Khusus ADMIN dan ANALIS)
+router.get('/', authMiddleware, checkRole(['admin', 'analis']), userController.getAllUsers);
 
 // c) Endpoint untuk semua user login
 router.get('/profile', authMiddleware, (req, res) => {
